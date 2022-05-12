66c46c4f6729c74a97c5c70f0c7f531781409b9f
Joan <joanjuz@gmail.com>
2022-05-11 21:17:43 -0600

    doublyLinkedList.h is working

--- src/test/tester.cpp
+++ src/test/tester.cpp
@@ -7,14 +7,18 @@
 #include "gui/controller.h"
 #include "utils/vectorStructure.h"
 #include "utils/vectorStructure.cpp"
-
+#include "utils/doublyLinkedList.h"
 
 #include <iostream>
 using namespace std;
 
 
 int Tester::testMainWindow(int argc, char **argv) {
+    doublyLinkedList<int>* linkedtest;
     vectorStructure<int> vectest;
+    linkedtest->append(&linkedtest,3);
+    linkedtest->insertNode(&linkedtest, 6);
+    linkedtest->printList(linkedtest);
     vectest.addElement(3);
     vectest.showVector();
     QApplication a(argc, argv);
