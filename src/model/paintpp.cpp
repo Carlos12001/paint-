c86a951127ab6a7517cf1f753cbb5ced8bff4138
Carlos12001 <carlos.andres12001@gmail.com>
2022-05-14 17:01:29 -0600

    Create method to get the currentImage.

--- src/model/paintpp.cpp
+++ src/model/paintpp.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "paintpp.h"
+
+void PaintPP::createImage(){
+    historyImage->append(&historyImage, getNextHistoryName());
+    currentImage = Image::createImageEmpty(1280,720);
+    return;
+}
+
+vectorStructure<Color> PaintPP::getCurrentImage() {
+    if(currentImage == nullptr){
+        Utilities::printMessageError("No exist current image. "
+                                     "I'm going to create the default image.");
+        createImage();
+    }
+    return currentImage->getColors();
+}
+
+string PaintPP::getNextHistoryName(){
+    return string("canvas_") + to_string(++counterImage) + string(".bmp");
+}
+
+PaintPP::~PaintPP() {
+
+}
--- src/model/paintpp.h
+++ src/model/paintpp.h
@@ -4,10 +4,30 @@
 
 #ifndef PAINT_PAINT_PP_H
 #define PAINT_PAINT_PP_H
-
+#include "iostream"
+using namespace std;
+#include "utils/doublyLinkedList.h"
+#include "utils/vectorStructure.h"
+#include "utils/utilities.h"
+#include "image.h"
 
 class PaintPP {
 
+    int counterImage = 0;
+
+    doublyLinkedList<string>* historyImage = nullptr;
+
+    Image* currentImage = nullptr;
+
+public:
+
+    vectorStructure<Color> getCurrentImage();
+
+    void createImage();
+
+    virtual ~PaintPP();
+
+    string getNextHistoryName();
 };
 
 
