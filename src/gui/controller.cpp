c1753f204c33dfd3401abab8cb2f82bb13ef9765
Carlos12001 <carlos.andres12001@gmail.com>
2022-05-15 11:55:02 -0600

    Paint the image in qt

--- src/gui/controller.cpp
+++ src/gui/controller.cpp
@@ -1,6 +1,6 @@
 #include "controller.h"
 #include "./ui_mainwindow.h"
-
+#include <QDesktopWidget>
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
 }
@@ -8,6 +8,8 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
 MainWindow::~MainWindow(){
     delete ui;
     delete paintPP;
+    delete mPainter;
+    delete canvasImage;
 }
 
 
@@ -24,5 +26,67 @@ void MainWindow::initGUIPaintPP(){
         paintPP = new PaintPP(width, height);
     }
 
+    canvasImage = new QImage(1280,720, QImage::Format_ARGB32_Premultiplied);
+    mPainter = new QPainter(canvasImage);
+
+    printCurrentImage();
+
     return;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+    painter.drawImage(minimumMoveX, minimumMoveY, *canvasImage);
+    event->accept();
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event) {
+    enablePainter = true;
+    mBegin = event->pos();
+    Utilities::printMessageInfo("CLICK!");
+    Utilities::printMessageInfo("Me muevo! En x: " + to_string(mBegin.rx()-minimumMoveX) + + " .En y: " + to_string(mBegin.ry()-minimumMoveY));
+    event->accept();
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event) {
+    if(!enablePainter){
+        event->accept();
+        return;
+    }else{
+//        QPen pen(Qt::white);
+//        pen.setCapStyle(Qt::RoundCap);
+//        pen.setWidth(10);
+//        mEnd = event->pos();
+//        mPainter->setPen(pen);
+//        mPainter->drawLine(mBegin, mEnd);
+        update();
+        event->accept();
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event) {
+    enablePainter = false;
+    event->accept();
+}
+
+void MainWindow::printCurrentImage(){
+    auto colors = paintPP->getColorOfCurrentImage();
+    int width =  paintPP->getWidthCanvas();
+    int height =  paintPP->getHeightCanvas();
+    cout << "(" << width << "," << height << ")" << "\t";
+    for (int j = 0; j < height; ++j) {
+        for (int i = 0; i < width; ++i) {
+            Color tempColor = colors.getElement(j*width+i);
+            auto r = (tempColor.r);
+            auto g = (tempColor.g);
+            auto b = (tempColor.b);
+            auto a = (tempColor.a);
+            QPen pen(QColor(r+0, g+0, b+0, a+0));
+            mPainter->setPen(pen);
+            mPainter->drawPoint(i, height-j);
+        }
+//        cout << endl;
+    }
+
+    update();
 }
--- src/gui/controller.h
+++ src/gui/controller.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_WINDOW_H
 #define MAIN_WINDOW_H
-
+#include <iostream>
+using namespace std;
+#include <QPainter>
+#include <QPaintEvent>
+#include <QMouseEvent>
+#include <QImage>
 #include "QMainWindow"
 #include "model/paintpp.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -11,6 +17,10 @@ class MainWindow : public QMainWindow{
     Q_OBJECT
 
 public:
+
+    static const int minimumMoveY = 100;
+
+    static const int minimumMoveX = 10;
 
     explicit MainWindow(QWidget *parent = nullptr);
 
@@ -24,5 +34,29 @@ private:
 
     PaintPP* paintPP = nullptr;
 
+    QPainter* mPainter;
+
+    QImage* canvasImage;
+
+    QWidget* canvasQWidget;
+
+    QPoint mBegin;
+
+    QPoint mEnd;
+
+    bool enablePainter = false;
+
+
+protected:
+
+    void paintEvent(QPaintEvent* event) override;
+
+    void mousePressEvent(QMouseEvent* event) override;
+
+    void mouseMoveEvent(QMouseEvent* event) override;
+
+    void mouseReleaseEvent(QMouseEvent* event) override;
+
+    void printCurrentImage();
 };
 #endif // MAIN_WINDOW_H
