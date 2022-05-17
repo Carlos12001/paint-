9b28350639c71a2721904af40747ad7125787006
Carlos12001 <carlos.andres12001@gmail.com>
2022-05-17 10:25:33 -0600

    DOcu

--- src/init_project/init_project.cpp
+++ src/init_project/init_project.cpp
@@ -5,6 +5,11 @@
 #include "init_project.h"
 #include "utils/utilities.h"
 #include "test/tester.h"
+/**
+ * gasdjdsjvpioxbnljd
+ * @param argc
+ * @param argv
+ */
 void InitProject::start(int argc, char* argv[]) {
     Tester::programWindow(argc, argv);
 }
